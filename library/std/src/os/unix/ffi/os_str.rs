8c90d167595bfdd4f72e7ac5410f0f2225af176a
Dan Gohman <dev@sunfishcode.online>
2021-11-19 14:29:35 -0800

    Implement `rustix::path::Arg` for `&Path` and `&OsStr`.

--- library/std/src/os/unix/ffi/os_str.rs
+++ library/std/src/os/unix/ffi/os_str.rs
@@ -1,3 +1,4 @@
+use crate::borrow::Cow;
 use crate::ffi::{OsStr, OsString};
 use crate::mem;
 use crate::sealed::Sealed;
@@ -64,5 +65,81 @@ impl OsStrExt for OsStr {
     #[inline]
     fn as_bytes(&self) -> &[u8] {
         &self.as_inner().inner
+    }
+}
+
+#[unstable(feature = "rustix", issue = "none")]
+impl rustix::path::Arg for &OsStr {
+    #[inline]
+    fn as_str(&self) -> rustix::io::Result<&str> {
+        self.to_str().ok_or(rustix::io::Error::INVAL)
+    }
+
+    #[inline]
+    fn to_string_lossy(&self) -> Cow<'_, str> {
+        OsStr::to_string_lossy(self)
+    }
+
+    #[inline]
+    fn as_cow_z_str(&self) -> rustix::io::Result<Cow<'_, rustix::ffi::ZStr>> {
+        Ok(Cow::Owned(
+            rustix::ffi::ZString::new(self.as_bytes())
+                .map_err(|_cstr_err| rustix::io::Error::INVAL)?,
+        ))
+    }
+
+    #[inline]
+    fn into_z_str<'b>(self) -> rustix::io::Result<Cow<'b, rustix::ffi::ZStr>>
+    where
+        Self: 'b,
+    {
+        self.as_bytes().into_z_str()
+    }
+
+    #[inline]
+    fn into_with_z_str<T, F>(self, f: F) -> rustix::io::Result<T>
+    where
+        Self: Sized,
+        F: FnOnce(&rustix::ffi::ZStr) -> rustix::io::Result<T>,
+    {
+        self.as_bytes().into_with_z_str(f)
+    }
+}
+
+#[unstable(feature = "rustix", issue = "none")]
+impl rustix::path::Arg for &crate::path::Path {
+    #[inline]
+    fn as_str(&self) -> rustix::io::Result<&str> {
+        self.as_os_str().to_str().ok_or(rustix::io::Error::INVAL)
+    }
+
+    #[inline]
+    fn to_string_lossy(&self) -> Cow<'_, str> {
+        crate::path::Path::to_string_lossy(self)
+    }
+
+    #[inline]
+    fn as_cow_z_str(&self) -> rustix::io::Result<Cow<'_, rustix::ffi::ZStr>> {
+        Ok(Cow::Owned(
+            rustix::ffi::ZString::new(self.as_os_str().as_bytes())
+                .map_err(|_cstr_err| rustix::io::Error::INVAL)?,
+        ))
+    }
+
+    #[inline]
+    fn into_z_str<'b>(self) -> rustix::io::Result<Cow<'b, rustix::ffi::ZStr>>
+    where
+        Self: 'b,
+    {
+        self.as_os_str().into_z_str()
+    }
+
+    #[inline]
+    fn into_with_z_str<T, F>(self, f: F) -> rustix::io::Result<T>
+    where
+        Self: Sized,
+        F: FnOnce(&rustix::ffi::ZStr) -> rustix::io::Result<T>,
+    {
+        self.as_os_str().into_with_z_str(f)
     }
 }
